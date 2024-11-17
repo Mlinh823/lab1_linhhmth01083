939c71c39c78aa5dfc7241878c8cf06753beb1f3
Hoàng Mai Linh <hoangmlinh3@gmail.com>
2024-11-17 17:36:50 +0700

    first commit

---
+++ SOF308_Vue/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <!-- <div><a>{{ username }}</a><a>{{ password }}</a></div> -->
+  <div>
+    <a v-html="username"></a>
+  </div>
+  <div>
+    <button v-on:click="changeUserName">Change username</button> 
+  </div>
+
+  <div>
+    <button v-bind:class="btnClass" v-on:click="changebtnClass">Dữ liệu v-Bind</button>
+  </div>
+
+  <div>
+    <button v-on:click="changeimgLink">Đổi ảnh</button>
+      <img v-bind:src="imgLink">
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: 'user01',
+      password: '1',
+      email: 'a@example.com',
+      btnClass: 'btn btn-success',
+      imgLink:'https://toigingiuvedep.vn/wp-content/uploads/2022/04/anh-hai-huoc.jpg'
+    }
+  },
+  methods:{
+      changeUserName()
+      {
+        this.username = 'New username'
+      },
+      changebtnClass(){
+        this.btnClass = 'btn btn-danger'
+      },
+      changeimgLink(){
+        this.imgLink = 'https://s.meta.com.vn/img/thumb.ashx/Data/image/2021/11/05/hinh-anh-meme-hai-huoc-anh-che-meme-16.jpgs'
+      }
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ SOF308_Vue/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="container">
+  <button class="btn btn-danger">Danger</button>
+  <button class="btn btn-warning">Warning</button>
+  <button class="btn btn-success">Success</button>
+  <button class="btn btn-info">Info</button>
+  <button class="btn btn-primary">Primary</button>
+  </div>
+  </template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
